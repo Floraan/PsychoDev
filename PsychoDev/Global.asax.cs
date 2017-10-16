1289ca2778993dd46d69fb462f4683ff2787a8fe
Floraan <floran.thoumelin@gmail.com>
2017-10-16 15:32:36 +0200

    auto redirect https

--- PsychoDev/Global.asax.cs
+++ PsychoDev/Global.asax.cs
@@ -1,9 +1,18 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 
 namespace PsychoDev
 {
     public class Global : HttpApplication
     {
+		protected void Application_BeginRequest(Object sender, EventArgs e)
+		{
+			if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+			{
+				Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"]
+			+ HttpContext.Current.Request.RawUrl);
+			}
+		}
         protected void Application_Start()
         {
         }
